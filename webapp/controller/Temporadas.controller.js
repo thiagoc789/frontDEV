789e0d3feab720c0a41396fccffcc9713a709e33
Santiago Castaño <santiago.castano@structum-co.com>
2023-06-24 23:20:14 +0000

    temporadas terminado faltan pruebas

--- webapp/controller/Temporadas.controller.js
+++ webapp/controller/Temporadas.controller.js
@@ -3,10 +3,57 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/m/MessageBox"
 ], function (Controller, MessageToast, MessageBox) {
+
+    function formatearFecha(fecha) {
+
+        var parts = fecha.split('/');
+        var month = parts[0];
+        var day = parts[1];
+        var year = parts[2];
+
+        // Añadir el prefijo "20" al año si es un valor de dos dígitos
+        if (year.length === 2) {
+            year = '20' + year;
+        }
+
+        if (month.length === 1) {
+            month = '0' + month;
+        }
+
+        if (day.length === 1) {
+            day = '0' + day;
+        }
+
+
+        // Crear una nueva fecha en formato YYYY-MM-DD
+        var formattedDate = year + '-' + month + '-' + day;
+
+        return formattedDate;
+
+    }
+    function getMonth(fecha) {
+
+        var parts = fecha.split('/');
+        var month = parts[0];
+
+
+        if (month.length === 1) {
+            month = '0' + month;
+        }
+
+        return month;
+
+    }
+    function getYear(fecha) {
+        var parts = fecha.split('/');
+        var year = parts[2];
+        if (year.length === 2) {
+            year = '20' + year;
+        }
+        return year;
+    }
     "use strict";
-
     return Controller.extend("evaluatorweb.controller.Temporadas", {
-
         onInit: function () {
             var that = this;
 
@@ -33,7 +80,6 @@ sap.ui.define([
             });
 
         },
-
 
         onButtonPress: function (oEvent) {
             var oModel = this.getView().getModel();
@@ -94,7 +140,50 @@ sap.ui.define([
                     }
                 }
             });
+        },
+
+        onCrearTemporada: function () {
+            var oDialog = this.byId("crearTemporadaDialog");
+            oDialog.open();
+        },
+
+
+        onSubmit: function () {
+            var oModel = this.getView().getModel()
+            var oNewEntry = {};
+            oNewEntry.id = this.getView().byId("id").getValue();
+
+            oNewEntry.start_date = formatearFecha(this.getView().byId("inicio").getValue());
+            oNewEntry.end_date = formatearFecha(this.getView().byId("fin").getValue());
+            oNewEntry.season_number = this.getView().byId("inputTemporada").getValue();
+            oNewEntry.status = 'cerrada';
+            oNewEntry.month = getMonth(this.getView().byId("inicio").getValue());
+            oNewEntry.year = getYear(this.getView().byId("inicio").getValue());
+
+            oModel.create("/Season", oNewEntry, {
+                success: function () {
+                    sap.m.MessageToast.show("Entrada creada con éxito");
+                    var oDialog = this.getView().byId("crearTemporadaDialog");
+                    this.getView().byId("id").setValue("");
+                    this.getView().byId("inicio").setValue("");
+                    this.getView().byId("fin").setValue("");
+                    this.getView().byId("inputTemporada").setValue("");
+                    oDialog.close();
+
+                }.bind(this),
+                error: function (oError) {
+                    sap.m.MessageToast.show("Error al crear la entrada: " + oError.message);
+                }
+            });
+
+        },
+
+        onCrearTemporadaDialogClose: function () {
+            var oDialog = this.getView().byId("crearTemporadaDialog");
+            oDialog.close();
         }
+
+
     });
 });
 

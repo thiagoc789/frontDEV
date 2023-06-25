7fb7d5295e8cb368c6e38ffa6a5e911e30153f91
Santiago Castaño <santiago.castano@structum-co.com>
2023-06-25 00:00:12 +0000

    se colapsa y se expande el panel, arreglar estilos de botones

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,64 +1,47 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/Popover"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, Popover, Button) {
-        "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Popover"
+], function (Controller, Popover) {
+    "use strict";
 
-        return Controller.extend("evaluatorweb.controller.main", {
-            onInit: function () {
-                var data = this.getOwnerComponent().getModel("data_model");
-                this.getView().setModel(data, "data");
-                //this._setToggleButtonTooltip(!Device.system.desktop); 
-            },
-            onItemSelect: function (oEvent) {
-                var oItem = oEvent.getParameter("item");
-                this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
-            },
+    return Controller.extend("evaluatorweb.controller.main", {
+        onInit: function () {
+            var data = this.getOwnerComponent().getModel("data_model");
+            this.getView().setModel(data, "data");
+            this._setToggleButtonTooltip(!sap.ui.Device.system.desktop);
+        },
 
-            handleUserNamePress: function (event) {
-                var oPopover = new Popover({
-                    showHeader: false,
-                    placement: PlacementType.Bottom,
-                    content: [
-                        new Button({
-                            text: 'Feedback',
-                            type: ButtonType.Transparent
-                        }),
-                        new Button({
-                            text: 'Help',
-                            type: ButtonType.Transparent
-                        }),
-                        new Button({
-                            text: 'Logout',
-                            type: ButtonType.Transparent
-                        })
-                    ]
-                }).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
+        onItemSelect: function (oEvent) {
+            var oItem = oEvent.getParameter("item");
+            this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+            this._toggleSideNavExpanded();
+        },
 
-                oPopover.openBy(event.getSource());
-            },
+        onSideNavButtonPress: function () {
+            this._toggleSideNavExpanded();
+        },
 
-            onSideNavButtonPress: function () {
-                var oToolPage = this.byId("toolPage");
-                var bSideExpanded = oToolPage.getSideExpanded();
+        _toggleSideNavExpanded: function () {
+            var oToolPage = this.byId("toolPage");
+            var bSideExpanded = oToolPage.getSideExpanded();
 
-                this._setToggleButtonTooltip(bSideExpanded);
+            this._setToggleButtonTooltip(!bSideExpanded);
 
-                oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
-            },
+            oToolPage.setSideExpanded(!bSideExpanded);
+        },
 
-            _setToggleButtonTooltip: function (bLarge) {
-                var oToggleButton = this.byId('sideNavigationToggleButton');
+        _setToggleButtonTooltip: function (bLarge) {
+            var oToggleButton = this.byId("sideNavigationToggleButton");
+            if (oToggleButton) {
                 if (bLarge) {
-                    oToggleButton.setTooltip('Large Size Navigation');
+                    oToggleButton.setTooltip("Expandir navegación");
                 } else {
-                    oToggleButton.setTooltip('Small Size Navigation');
+                    oToggleButton.setTooltip("Colapsar navegación");
                 }
             }
-
-        });
+        },
+        
     });
+});
+
+

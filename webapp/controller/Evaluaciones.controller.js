47971a3352fecb449f0934a01a57631fd2d6bf62
Santiago Castaño <santiago.castano@structum-co.com>
2023-06-30 06:12:37 +0000

    ahora si funcionando tabla evaluaciones aprendido lo de las tables para elegir, voy a hacer lo de editar

--- webapp/controller/Evaluaciones.controller.js
+++ webapp/controller/Evaluaciones.controller.js
@@ -3,26 +3,26 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/m/MessageBox",
     "sap/ui/model/json/JSONModel",
-], function(Controller, MessageToast, MessageBox, JSONModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel) {
     "use strict";
 
     return Controller.extend("evaluatorweb.controller.Evaluaciones", {
         formatter: {
-            weightInPercentage: function(sWeight) {
+            weightInPercentage: function (sWeight) {
                 if (!sWeight) {
                     return "0%";
                 }
                 return parseFloat(sWeight * 100).toFixed(0) + "%";
             }
         },
-        onInit: function() {
+        onInit: function () {
             var oButton = this.getView().byId("buttonCancel"); // reemplace "myButtonId" con el id real de tu botón
             oButton.attachPress(this.onCrearEvaluacionDialogClose.bind(this));
 
             var oSmartTable = this.getView().byId("LineItemsSmartTable"); // obtener la referencia de SmartTable
             var oTable = oSmartTable.getTable(); // obtener la referencia de la tabla interna
 
-            
+
         },
 
         onOpenCreateDialog: function () {
@@ -48,7 +48,7 @@ sap.ui.define([
             oNewEntry.name = this.getView().byId("inputName").getValue();
             oNewEntry.description = this.getView().byId("inputDescription").getValue();
             var sWeight = this.getView().byId("inputWeight").getValue();
-    oNewEntry.weight = sWeight ? parseFloat(sWeight) / 100 : 0;
+            oNewEntry.weight = sWeight ? parseFloat(sWeight) / 100 : 0;
 
             oModel.create("/Evaluation", oNewEntry, {
                 success: function () {
@@ -68,25 +68,34 @@ sap.ui.define([
 
         },
 
+        onToggle: function (oEvt) {
+
+            var editable = this.getView().byId("LineItemsSmartTable").getEditable();
+            this.getView().getModel().setProperty("/smiEditable", editable);
+            console.log(oEvt)
+            console.log(editable)
+
+        },
+
         onEliminarUsuario: function () {
-            var oModel = this.getView().getModel(); // Obtener la referencia del modelo
-            var oSmartTable = this.getView().byId("LineItemsSmartTable"); // Obtener la referencia de SmartTable
-            var oTable = oSmartTable.getTable(); // Obtener la referencia de la tabla interna
-            var oTable = this.getView().byId("_IDGenTable1");
-            var aSelectedContexts = oTable.getSelectedContexts(); // Obtener los contextos de las filas seleccionadas
+            var oModel = this.getView().getModel(); // obtener la referencia del modelo
+            var oSmartTable = this.getView().byId("LineItemsSmartTable"); // obtener la referencia de SmartTable
+            var oTable = oSmartTable.getTable(); // obtener la referencia de la tabla interna
+            var aSelectedIndices = oTable.getSelectedIndices(); // obtener los índices de las filas seleccionadas
         
-            // Implementar la lógica de eliminar filas
+            if (aSelectedIndices.length === 0) {
+                // No hay filas seleccionadas, no hay nada que eliminar
+                return;
+            }
+        
             MessageBox.confirm("¿Estás seguro de que quieres eliminar las evaluaciones seleccionadas?", {
                 onClose: function (sButton) {
                     if (sButton === MessageBox.Action.OK) {
-                        // Recorrer cada contexto seleccionado
-                        aSelectedContexts.forEach(function (oContext) {
-                            var sPath = oContext.sPath; // Obtener el path del contexto
-        
-                            // Eliminar el elemento en el modelo utilizando el path
+                        aSelectedIndices.forEach(function (iIndex) {
+                            var sPath = oTable.getContextByIndex(iIndex).getPath();
                             oModel.remove(sPath, {
                                 success: function () {
-                                    MessageToast.show("Evaluación eliminada con éxito");
+                                    MessageToast.show("Evaluación eliminada con exito");
                                     oModel.refresh(true);
                                 },
                                 error: function (oError) {
@@ -97,7 +106,7 @@ sap.ui.define([
                     }
                 }
             });
-        }
+        },
 
     });
 });
